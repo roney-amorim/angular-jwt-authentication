6befe17352b68d05459107d5738889293eba5c37
roney-amorim <roney.augusto.amorim@gmail.com>
2019-04-09 02:27:47 -0300

    ajuste url environment

--- src/app/clientes/clientes.service.ts
+++ src/app/clientes/clientes.service.ts
@@ -9,24 +9,26 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class ClientesService {
+
   BASE_URL: string;
+
   constructor(private http: HttpClient) {
     this.BASE_URL = environment.url;
-   }
+  }
 
-   salvar(cliente:Cliente): Observable<Cliente>{
-    return this.http.post<Cliente>(this.BASE_URL.concat('/clientes'), cliente).pipe(map(json =>{ return json }));
+  salvar(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.BASE_URL.concat('/clientes'), cliente).pipe(map(json => { return json }));
   }
-  buscarPorCodigo(codigo:number): Observable<Cliente>{
-    return this.http.get<Cliente>(this.BASE_URL.concat(`/clientes/${codigo}`)).pipe(map(json =>{ return json }));
+  buscarPorCodigo(codigo: number): Observable<Cliente> {
+    return this.http.get<Cliente>(this.BASE_URL.concat(`/clientes/${codigo}`)).pipe(map(json => { return json }));
   }
-  consultar(): Observable<Cliente[]>{
-    return this.http.get<Cliente[]>(this.BASE_URL.concat('/clientes')).pipe(map(json =>{ return json }));
+  consultar(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(this.BASE_URL.concat('/clientes')).pipe(map(json => { return json }));
   }
-  alterar(cliente:Cliente): Observable<Cliente>{
-    return this.http.patch<Cliente>(this.BASE_URL.concat('/clientes'), cliente).pipe(map(json =>{ return json }));
+  alterar(cliente: Cliente): Observable<Cliente> {
+    return this.http.patch<Cliente>(this.BASE_URL.concat('/clientes'), cliente).pipe(map(json => { return json }));
   }
-  deletar(codigo: number): Observable<Cliente>{
-    return this.http.delete<Cliente>(this.BASE_URL.concat('/clientes/' + codigo)).pipe(map(json =>{ return json }));
+  deletar(codigo: number): Observable<Cliente> {
+    return this.http.delete<Cliente>(this.BASE_URL.concat(`/clientes/${codigo}`)).pipe(map(json => { return json }));
   }
 }
